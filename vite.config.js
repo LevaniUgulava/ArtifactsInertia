77dbed99a6eeb0612182e13f568f3dd1e537a9c8
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2026-09-08 17:49:53 +0400

    chore: add React plugin to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import inertia from '@inertiajs/vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        react(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
